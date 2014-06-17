06a61d5aa77f03aa7f6078e40f12f263d2c9a1a6
AnupamBapat <anupam132@gmail.com>
2014-06-17 10:15:17 +0530

    Update Register.js

--- Friends/scripts/app/Register.js
+++ Friends/scripts/app/Register.js
@@ -1,46 +1,38 @@
-/**
- * Signup view model
- */
 var app = app || {};
 
 app.Trial = (function () {
     'use strict';
-
+  
     var viewModel = kendo.observable({
-        name: "",
-        surname: "",
+        FirstName: "",
+        LastName: "",
         address: "",
-        email: "",
-
+        EmailId: "",
+        ContactNumber: "",
+        DOB: "",
+        Experience:"",
+        CurrentOrganization:"",
+        CurrentSalary: "",
+        ExpectedSalary: "",
+        NoticePeriod: "",
+       
 
         updateUserDetails: function () {
-
-
-          
             jQuery.support.cors = true;
-            //var updateOptions = {
-
-            //    url: "http://localhost:52812/api/recruit/SaveInterviewDetails",
-            //    Type: "POST",
-            //    dataType: "JSON",
-            //    data: JSON.stringify(name = ""),
-            //    contentType: "application/json"
-            //}
-            var testData = {                
+          
+            var testData = {
                 FirstName: viewModel.get("name"),
                 LastName: viewModel.get("lastname"),
-                //address: viewModel.get("address")
-                //email: viewModel.get("expectedsalary"),
-                //email: viewModel.get("currentsalary"),
+                address: viewModel.get("address"),
                 EmailId: viewModel.get("email"),
-                //email: viewModel.get("contact"),
-                //email: viewModel.get("dob"),
-                //email: viewModel.get("experience"),
-                //email: viewModel.get("currentcompany"),
-                //email: viewModel.get("currentsalary"),
-                //email: viewModel.get("expectedsalary"),
-                //email: viewModel.get("noticeperiod")
-};
+                ContactNumber: viewModel.get("contact"),
+                DOB: viewModel.get("dob"),
+                Experience: viewModel.get("experience"),
+                CurrentOrganization: viewModel.get("currentcompany"),
+                CurrentSalary: viewModel.get("currentsalary"),
+                ExpectedSalary: viewModel.get("expectedsalary"),
+                NoticePeriod: viewModel.get("noticeperiod")
+            };
             $.ajax({
                 type: "POST",
                 data: JSON.stringify(testData),
@@ -49,126 +41,55 @@ app.Trial = (function () {
                 //  });                
 
                 success: function (data) {
-                    if(data){
-                    Alert("Test");
+                    if (data) {
+                        alert("Test");
                     }
-                    
-
-
                 },
 
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
-                  
+
                     alert(textStatus + ":" + errorThrown);
                 }
             });
+           
 
-        }
-    });
+        },
+        updatedropdowndetails: function () {
+            jQuery.support.cors = true;
+           
+        $.ajax({
+            
+            type: "GET",
+            url: "http://localhost:52812/api/Candidate/GetDriveDetails",
+            contentType: "application/json",
+            
+            success: function (data) {
+               
+                var ddl = $("#signupHighlevelPicker");
+                var dataSource = [];
+                for (i = 0; i < data.length; i++) {
+                  //  dataSource.push({ "DriveName": data[i].DriveName })
+                    ddl.append($("<option></option>").val(data[i].DriveName).html(data[i].DriveName));
+                };
+               // OrderUomTypes: dataSource
+                //$("#ddlCompany").kendoMobileDropDownList({
+                //    dataTextField: "DriveName",
+                //    dataValueField: "DriveName",
+                //    dataSource: dataSource
+                //});
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                
+        
+            }
+        });
+}
+        
 
-
+});
     kendo.bind($("#signup-form"), viewModel);
-    //})();
-
-
-
-    //var viewModel = kendo.observable({
-    //        name: "",
-    //        lastname: "",
-    //        expectedsalary: "",
-    //        currentsalary: "",
-    //        email: "",
-    //        contact: "",
-    //        dob: "",
-    //        experience: "",
-    //        currentcompany: "",         
-    //        noticeperiod: "",
-    //        updateUserDetails: function () {
-
-
-    //            debugger;
-    //            jQuery.support.cors = true;
-    //            var getdata = {
-    //                FirstName: viewModel.get("name"),
-    //                LastName: viewModel.get("lastname"),
-    //                //address: viewModel.get("address")
-    //                //email: viewModel.get("expectedsalary"),
-    //                //email: viewModel.get("currentsalary"),
-    //                EmailId: viewModel.get("email"),
-    //                //email: viewModel.get("contact"),
-    //                //email: viewModel.get("dob"),
-    //                //email: viewModel.get("experience"),
-    //                //email: viewModel.get("currentcompany"),
-    //                //email: viewModel.get("currentsalary"),
-    //                //email: viewModel.get("expectedsalary"),
-    //                //email: viewModel.get("noticeperiod")
-    //            };
-    //            $.ajax({
-
-    //                type: "POST",
-    //                data: JSON.stringify(getdata),
-    //                url: "http://localhost:52812/api/recruit/InsertCandidateDetails",
-    //                contentType: "application/json",
-    //                success: function (data) {
-    //                    alert("Test");
-    //                    debugger;
-    //                }
-
-    //            });
-
-
-    //        }
-
-    //        //updateUserDetails: function () {
-
-
-    //        //    debugger;
-    //        //    jQuery.support.cors = true;
-    //        //    var getdata = {
-    //        //        name: "abc",//viewModel.get("name"),
-    //        //        surname: "a",//viewModel.get("lastname"),
-    //        //        address: "aa",//viewModel.get("address"),
-    //        //        email: "a"//viewModel.get("expectedsalary"),                    
-    //        //        //email: viewModel.get("currentsalary"),
-    //        //        //email: viewModel.get("email"),
-    //        //        //email: viewModel.get("contact"),
-    //        //        //email: viewModel.get("dob"),
-    //        //        //email: viewModel.get("experience"),
-    //        //        //email: viewModel.get("currentcompany"),
-    //        //        //email: viewModel.get("currentsalary"),
-    //        //        //email: viewModel.get("expectedsalary"),
-    //        //        //email: viewModel.get("noticeperiod")
-    //        //    };
-    //        //    $.ajax({
-    //        //        type: "POST",
-    //        //        data: JSON.stringify(getdata),
-    //        //        url: "http://localhost:52812/api/Candidate/InsertCandidateDetails",
-    //        //        contentType: "application/json",
-                    
-
-    //        //        success: function (data) {
-                     
-    //        //            if (data == true) {
-    //        //                alert('User Registered successfully...');
-    //        //            }
-    //        //            else
-    //        //                alert('Unable to register');
-
-
-
-    //        //        },
-    //        //        error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    
-    //        //            alert(textStatus + ":" + errorThrown);
-    //        //        }
-    //        //    });
-
-    //        //}
-    //    });
-
-
-    //    kendo.bind($("#signUp"), viewModel);
-
-        return viewModel;
+    return viewModel;
 
 }());
+
+
